856a93c90f8b0b54b519ca91443f493f62ce1f4d
joshnicholas <hi@thambi.li>
2023-05-10 08:01:29 +1000

    Try new path

--- app.py
+++ app.py
@@ -24,10 +24,13 @@ def get_csv():
     except:
         return "You haven't specificed a file that exists"
     
-    csv_path = f"./static/{month}.csv"   
+    csv_path = f"static/{month}.csv"   
 
     # Also make sure the requested csv file does exist
     if not os.path.isfile(csv_path):
         return f"ERROR: file {month} was not found on the server"
     # Send the file back to the client
     return send_file(csv_path, as_attachment=True, attachment_filename=f'{month}.csv')
+
+
+https://thambili.herokuapp.com/get_data/file=latest_foi
