88e83a79182fc8e2f701bfc1b40c8c73d98dc0be
joshnicholas <hi@thambi.li>
2023-05-10 15:54:15 +1000

    abs scraper

---
+++ foi_scrapers/services_aus.py
@@ -0,0 +1,135 @@
+# %%
+import pandas as pd 
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+import datetime 
+import pytz
+import sys
+
+import os 
+import pathlib
+
+pathos = pathlib.Path(__file__).parent
+os.chdir(pathos)
+
+# %%
+
+##
+def dumper(path, name, frame):
+    with open(f'{path}/{name}.csv', 'w') as f:
+        frame.to_csv(f, index=False, header=True)
+
+##
+def rand_delay(num):
+  import random 
+  import time 
+  rando = random.random() * num
+  print(rando)
+  time.sleep(rando)
+
+def already_done(pathos, stemmo):
+
+    if pathos[-1] != '/':
+        pathos += '/'
+
+    if f'{stemmo}.csv' in os.listdir(pathos):
+        inter = pd.read_csv(f"{pathos}{stemmo}.csv")
+        uniques = inter['Id'].unique().tolist()
+        return uniques
+    else:
+        return []
+
+def create_raw_append_csv(pathos, nammo, new_record, drop_col, sort_col):
+
+    new = pd.DataFrame.from_records([new_record])
+
+    if pathos[-1] != '/':
+        pathos += '/'
+    # print("cwd:", os.getcwd())
+    fillos = os.listdir(pathos)
+    # print(fillos)
+    if f'{nammo}.csv' not in fillos:
+        # print("if")
+        dumper(pathos, nammo, new)
+    
+    else:
+        # print("else")
+        old = pd.read_csv(f"{pathos}{nammo}.csv")
+
+        tog = pd.concat([new, old])
+        tog.drop_duplicates(subset=[drop_col], inplace=True)
+        tog.sort_values(by=[sort_col], ascending=False, inplace=True)
+        
+        dumper(pathos, nammo, tog)
+    
+
+#%%
+
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
+"Referer": 'https://www.google.com',
+"DNT":'1'}
+
+urlo = 'https://www.servicesaustralia.gov.au/freedom-information-disclosure-log'
+home = urlo
+r = requests.get(urlo, headers=headers)
+
+soup = bs(r.text, 'html.parser')
+
+
+# %%
+
+rows = soup.find_all('tr')
+
+donners = already_done('../data/foi', 'services_aus')
+# print("donners: ", donners)
+
+for row in rows[1:]:
+
+    # print(row)
+
+    try:
+
+        stemmo = row.find(attrs={"headers":"view-field-reference-table-column"}).text.strip()
+        # print(stemmo)
+
+        if stemmo not in donners:
+        
+            datto = row.find(class_='datetime').text.strip()
+            datto = datetime.datetime.strptime(datto, "%d %B %Y")
+            datto = datto.strftime("%Y-%m-%d")
+            # print(datto)
+
+            title = row.find(attrs={"headers":"view-field-summary-table-column"}).text.strip()
+            # print(title)
+
+            # urlo = row.find(attrs={"headers":"view-title-table-column"}).a['href']
+            urlo = row.find(class_='file--application-pdf').a['href']
+            # print(urlo)
+
+            file = 'https://www.dcceew.gov.au/' + urlo
+            # print(file)
+
+            record = {"Agency": "Services Australia",
+                    "Date": datto,
+                    "Id": stemmo,
+                    "Title": title,
+                    "Url": urlo,
+                    "Home_url": home,
+                    "File": file}
+            
+            # print(record)
+            create_raw_append_csv('../data/foi', 'services_aus', record, "Id", 'Date')
+
+    except AttributeError as e:
+
+        print(urlo)
+        print(f"Exception is {e}")
+        print(f"Line: {sys.exc_info()[-1].tb_lineno}")
+        continue
+
+
+    
+# %%
---
+++ other_scrapers/abs_calender_scraper.py
@@ -0,0 +1,157 @@
+# %%
+import pandas as pd 
+import requests
+import os 
+import pathlib
+from bs4 import BeautifulSoup as bs 
+
+import time 
+
+import datetime
+from dateutil.relativedelta import relativedelta
+import pytz
+
+from selenium import webdriver 
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Firefox(options=chrome_options)
+
+# %%
+
+##
+def dumper(path, name, frame):
+    with open(f'{path}/{name}.csv', 'w') as f:
+        frame.to_csv(f, index=False, header=True)
+
+##
+def rand_delay(num):
+  import random 
+  import time 
+  rando = random.random() * num
+  print(rando)
+  time.sleep(rando)
+
+##
+def if_no_fold_create(pathos, to_check):
+    if pathos[-1] != '/':
+        pathos += '/'
+
+    folds = os.listdir(pathos)
+
+    if to_check not in folds:
+        os.mkdir(f"{pathos}{to_check}")
+
+def create_raw_append_csv(pathos, nammo, new_record, drop_cols, sort_col):
+
+    new = pd.DataFrame.from_records([new_record])
+
+    if pathos[-1] != '/':
+        pathos += '/'
+    # print("cwd:", os.getcwd())
+    fillos = os.listdir(pathos)
+
+    if f'{nammo}.csv' not in fillos:
+        # print("if")
+        dumper(pathos, nammo, new)
+    
+    else:
+        # print("else")
+        old = pd.read_csv(f"{pathos}{nammo}.csv")
+
+        tog = pd.concat([new, old])
+        tog.drop_duplicates(subset=drop_cols, inplace=True)
+        tog.sort_values(by=[sort_col], ascending=False, inplace=True)
+        dumper(pathos, nammo, tog)
+
+# %%
+
+today = datetime.datetime.now()
+scrape_date_stemmo = today.astimezone(pytz.timezone("Australia/Brisbane")).strftime('%Y%m%d')
+scrape_hour = today.astimezone(pytz.timezone("Australia/Brisbane")).strftime('%H')
+
+
+# %%
+### Get this month and next month for the scraper
+
+next_month = today + relativedelta(months=1)
+next_month = next_month.strftime("%Y%m")
+
+this_month = today.strftime("%Y%m")
+
+
+# %%
+
+# %%
+
+pathos = pathlib.Path(__file__).parent
+os.chdir(pathos)
+
+#%%
+
+listo = []
+
+for month in [this_month, next_month]:
+
+    if month not in os.listdir('../archive/abs'):
+
+        urlo = f'https://www.abs.gov.au/release-calendar/future-releases/{month}'
+
+        home = urlo 
+        driver.get(urlo)
+
+        time.sleep(5)
+
+        soup = bs(driver.page_source, 'html.parser')
+
+        box = soup.find(class_="view-content")
+
+
+        rows = box.find_all(class_='views-row')
+
+        for row in rows:
+            # print(row)
+
+            datto = row.find(class_='datetime').text.strip()
+            datto = datetime.datetime.strptime(datto, "%A %d %B %Y %H:%M%p %Z")
+            datto = datto.strftime("%Y-%m-%d")
+            # print(datto)
+
+            title = row.find(class_='event-name').text.strip()
+            # print(title)
+
+            desc = row.find(class_='views-field-body').text.strip()
+            # print(desc)
+
+            ref = row.find(class_='reference-period-value').text.strip()
+            # print(ref)
+
+            try:
+                urlo = 'https://www.abs.gov.au' + row.a['href']
+            except:
+                urlo = ''
+            # print(urlo)
+
+            record = {"Release": title,
+                    "Description": desc,
+                    "Date": datto,
+                    "Url": urlo}
+            
+            listo.append(record)
+
+            create_raw_append_csv('../archive/abs', month, record, ["Release", 'Date'], 'Date')            
+
+# print(rows)
+# %%
+
+cat = pd.DataFrame.from_records(listo)
+
+with open(f'../static/latest_abs.json', 'w') as f:
+    cat.to_json(f, orient='records')
+
+# %%
