b2163af0fd92dfd3bdbe0129e3e139e1483608d6
Josh Nicholas <hi@thambi.li>
2023-09-01 17:23:52 +1000

    Fix wikitrends

--- trend_scrapers/wikitrends.py
+++ trend_scrapers/wikitrends.py
@@ -143,11 +143,14 @@ utc_hour = utc_then.strftime('%H')
 # %%
 
 wiki_linko = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia.org/all-access/{utc_year}/{utc_month}/{utc_day}"
-https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=last-month&excludes=#
-https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=last-month&excludes=#
-https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=yesterday&excludes=#
+# https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=last-month&excludes=#
+# https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=last-month&excludes=#
+# https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=yesterday&excludes=#
 
-https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=2015-07-01&excludes=#
+# https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=2015-07-01&excludes=#
+# https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=yesterday&excludes=#
+
+# wiki_linko = f"https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date={utc_year}-{utc_month}-{utc_day}&excludes=#"
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
 'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
@@ -158,6 +161,15 @@ wiki_r = requests.get(wiki_linko, headers=headers)
 
 # print(wiki_r.status_code)
 # print(wiki_r.url)
+# %%
+
+print(wiki_r.text)
+
+# %%
+
+
+
+# %%
 
 if wiki_r.status_code != 404:
 
@@ -173,6 +185,8 @@ if wiki_r.status_code != 404:
     df = pd.DataFrame(wiki_trends)
     df = df.rename(columns={0: "Page"})
     df['Page'] = df['Page'].str.replace("_", " ")
+
+
 
 
 
